69d3931e7c7335627a1e4d797ae0b5d45ab8d851
ArtemVaska <artem_vasilev_01@list.ru>
2024-10-17 18:55:19 +0300

    Add nucl_search and save_esearch_results functions

--- Scripts/functions.py
+++ Scripts/functions.py
@@ -1,3 +1,10 @@
+import os
+
+
+from Bio import Entrez
+Entrez.email = "artem_vasilev_01@list.ru"
+
+
 def calculate_n_fasta_lines(line: str, fasta_line_length) -> int:
     """
     Additional function for plain_to_fasta function
@@ -35,3 +42,31 @@ def plain_to_fasta(file: str,
                 for n_fasta_line in range(n_fasta_lines):
                     outfile.write(line[n_fasta_line * fasta_line_length : (n_fasta_line+1) * fasta_line_length] + "\n")
                 outfile.write(line[-extra_line:] + "\n")
+
+
+def nucl_search(query: str) -> list:
+    """
+    Searches smth via Entrez.esearch in the nucleotide database
+
+    query example: (Drosophilidae[ORGN] NOT Drosophila melanogaster[ORGN]) AND \
+    (chromosome X[WORD] NOT PREDICTED[WORD] NOT gene[WORD]) AND 15000000:75000000[SLEN]
+    """
+    handle = Entrez.esearch(db="nucleotide", term=query, retmax=100)
+    record = Entrez.read(handle)
+    return record["IdList"]
+
+
+def save_esearch_results(id_list: list, folder: str) -> None:
+    """
+    Saves .fa files in folder from esearch results
+    """
+    SAVE_DIR = "/home/artemvaska/Master_degree/Diploma/References"
+    save_path = os.path.join(SAVE_DIR, folder)
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    for seq_id in id_list:
+        with open(f"{save_path}/{seq_id}.fa", "w") as outfile:
+            seq = Entrez.efetch(db="nucleotide", id=seq_id, retmode="text", rettype="fasta").read()
+            outfile.write(seq)
