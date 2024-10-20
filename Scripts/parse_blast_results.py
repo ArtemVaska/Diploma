5fa0f36836f9c4b932e991eb87d1f40853baa1f9
ArtemVaska <artem_vasilev_01@list.ru>
2024-10-20 18:58:10 +0300

    Parse functions.py into 3 .py files; add new functions for blast parse

---
+++ Scripts/parse_blast_results.py
@@ -0,0 +1,58 @@
+import Bio.Blast
+
+
+def extract_target_range(hit: Bio.Blast.Hit) -> list:
+    """
+    Extracts hit range in target
+
+    :param hit:
+    :return: list with coords [start, end]
+    """
+    target_range_list = []
+
+    for hsp in hit:
+        target_range_list.append(int(hsp.coordinates[0][0]))
+        target_range_list.append(int(hsp.coordinates[0][-1]))
+
+    target_range = [min(target_range_list), max(target_range_list)]
+
+    return target_range
+
+
+def extract_target_id(hit: Bio.Blast.Hit) -> str:
+    """
+    Extracts target id in hit. Additional func for calculate_qc
+
+    :param hit:
+    :return:
+    """
+    full_target_id = hit.target.id
+    id_type = full_target_id.split("|")[0]
+    if "gi" not in id_type:
+        split_target_id = full_target_id.split("|")[1]
+    else:
+        split_target_id = full_target_id.split("|")[3]
+
+    return split_target_id
+
+
+def calculate_qc(blast_record: Bio.Blast.Record) -> dict:
+    """
+    Calculates query coverage for every hit in blast_record
+
+    :param blast_record:
+    :return: dict target.id:QC
+    """
+    query_length = len(blast_record.query)
+    qcs = {}
+
+    for hit in blast_record:
+        # target_id = extract_target_id(hit)  # possible formatting (only Acc.)
+        target_id = hit.target.id
+        query_total_range = 0
+        for hsp in hit:
+            query_total_range += int(hsp.coordinates[1][-1]) - int(hsp.coordinates[1][0])
+        qc = query_total_range / query_length
+        qcs[target_id] = round(qc, 4)
+
+    return qcs
