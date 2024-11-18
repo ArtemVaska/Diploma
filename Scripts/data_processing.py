9598600f2b90bb85eb3198d37c41b10083ae4cbd
ArtemVaska <artem_vasilev_01@list.ru>
2024-11-18 13:04:34 +0300

    Add save_seq and save_seqs functions

--- Scripts/data_processing.py
+++ Scripts/data_processing.py
@@ -1,7 +1,12 @@
+import os
+import time
 import pandas as pd
 import numpy as np
 
 from sklearn.cluster import DBSCAN
+from Bio import Entrez
+
+Entrez.email = "artem_vasilev_01@list.ru"
 
 
 def cluster_analysis(qcs: dict, qc_threshold=0.1) -> pd.DataFrame:
@@ -23,3 +28,53 @@ def cluster_analysis(qcs: dict, qc_threshold=0.1) -> pd.DataFrame:
     qcs_df["Cluster"] = labels
 
     return qcs_df
+
+
+def save_seq(save_dir: str, folder: str, subfolder: str, acc: str, strand: int, seq_start: int, seq_stop: int) -> None:
+    """
+    Saves sequence obtained with Entrez.efetch in the specified folder
+    Additional function for save_seqs
+
+    :param save_dir:
+    :param folder:
+    :param subfolder:
+    :param acc:
+    :param strand:
+    :param seq_start:
+    :param seq_stop:
+    :return:
+    """
+
+    save_path = os.path.join(save_dir, folder, subfolder)
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    with open(f"{save_path}/{acc}.fa", "w") as outfile:
+        seq = Entrez.efetch(db="nucleotide", id=acc, strand=strand, seq_start=seq_start, seq_stop=seq_stop,
+                            rettype="fasta").read()
+        outfile.write(seq)
+
+
+def save_seqs(folder: str, df: pd.DataFrame,
+              save_dir: str = "/home/artemvaska/Master_degree/Diploma/Sequences") -> None:
+    """
+    Saves sequences from dataframe
+
+    :param folder:
+    :param df:
+    :param save_dir:
+    :return:
+    """
+    for cluster in range(len(df.Cluster.unique())):
+        subset_df = df.loc[df["Cluster"] == cluster]
+        min_value = str(df.loc[df["Cluster"] == cluster].min().QC.round(2))
+        max_value = str(df.loc[df["Cluster"] == cluster].max().QC.round(2))
+        subfolder = f"{min_value}-{max_value}"
+
+        for hit in subset_df.index:
+            line = df.loc[hit]
+            acc, strand, seq_start, seq_stop = line.Acc, line.Strand, line.Start, line.Stop
+            time.sleep(1)
+            save_seq(save_dir, folder, subfolder, acc, strand, seq_start, seq_stop)
+            
